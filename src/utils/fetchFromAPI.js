6b3b39744d4e21f9ba7fda41ecd81bb525a63c3f
oletilwe12 <loatemophuting@gmail.com>
2022-08-28 03:37:58 +0200

    third commit

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -6,13 +6,16 @@ const options = {
   method: 'GET',
   url: BASE_URL,
   params: {
-    relatedToVideoId: '7ghhRHRP6t4',
-    part: 'id,snippet',
-    type: 'video',
     maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': '8c866e57e8msh2f396bc5fa3ced4p1f2807jsnab7457a3f717',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
 };
