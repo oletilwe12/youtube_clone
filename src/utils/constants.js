a90413e10829d1498e77f07a473400c59db17641
oletilwe12 <loatemophuting@gmail.com>
2022-09-29 13:53:58 +0200

    fireship

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,8 +16,8 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
-  { name: 'FreeCodeCamp', icon: <CodeIcon />, },
+  { name: 'Fireship', icon: <CodeIcon />, },
+  { name: 'FreeCode', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
